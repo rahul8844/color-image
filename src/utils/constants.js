ee159fa555c86f0073d26792bb3f2c18641f7e99
Rahul Gupta <rahulgupta8844@gmail.com>
2021-09-20 15:59:05 +0530

    added workflow

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,6 @@ export const IMAGE_SIZE={
     width: 256,
     height: 128
 };
-export const STEP_DIFF = 8;
+export const STEP_DIFF = 5;
 export const RANGE = 256;
 export const COlOR_DIVISION = RANGE/STEP_DIFF;
