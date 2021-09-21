01ddf9d80659d22d1935ebe662cda69972952ae4
Rahul Gupta <rahulgupta8844@gmail.com>
2021-09-21 18:16:31 +0530

    download button

--- src/components/ImageFormation.jsx
+++ src/components/ImageFormation.jsx
@@ -1,26 +1,43 @@
 import { COLOR_DIVISION, STEP_DIFF } from "../utils/constants";
 import { getImageUrl } from "../utils/util";
+import { saveAs } from 'file-saver'
+import { useEffect, useState } from "react";
+import './styles.css';
 
 /** Image Component */
 const ImageFormation =(props)=>{
-    const colorPoints = [];
-    const colors = [];
-    /** Finding equally disributed numbers which lies in range 0 - 255 */
-    for (let i = 1; i <= COLOR_DIVISION; i++) {
-        colorPoints.push(i*STEP_DIFF - 1 );
+    const [imageUrl, setImageUrl] = useState('');
+    
+    useEffect(()=>{
+        const colorPoints = [];
+        const colors = [];
+        /** Finding equally disributed numbers which lies in range 0 - 255 */
+        for (let i = 1; i <= COLOR_DIVISION; i++) {
+            colorPoints.push(i * STEP_DIFF - 1);
+        }
+
+        /** Getting all the colors which can be made from above 32 r g b combination */
+        colorPoints.forEach(r => {
+            colorPoints.forEach(g => {
+                colorPoints.forEach(b => {
+                    colors.push([r, g, b]);
+                });
+            })
+        });
+        setImageUrl(getImageUrl(colors));
+    },[]);
+
+    const downloadImage = () => {
+        saveAs(imageUrl, 'image.jpg') // Put your image url here.
     }
 
-    /** Getting all the colors which can be made from above 32 r g b combination */
-    colorPoints.forEach(r => {
-        colorPoints.forEach(g => {
-            colorPoints.forEach(b => {
-                colors.push([r, g, b]);
-            });
-        })
-    });
     return(
         <div id="image-html">
-            <img src={getImageUrl(colors)}/>
+            <button 
+                onClick={downloadImage} 
+                style={{marginBottom: '5px',cursor: 'pointer', padding: '5px'}}
+            >Download Image</button>
+            <img src={imageUrl}/>
         </div>
     )
 }
